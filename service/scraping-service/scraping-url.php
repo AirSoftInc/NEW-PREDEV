995169e7aff743e96b6917780f82d1abd085c893
Paul Chávez <cpchavez.airsoft@outlook.com>
2019-06-08 14:02:12 -0500

    PRE-012
    Se des comentaron los periódicos.

--- service/scraping-service/scraping-url.php
+++ service/scraping-service/scraping-url.php
@@ -85,17 +85,16 @@
     } 
 
     // //This method is for scraping for the new Diario de Xalapa
-    // function getNewsFromDiarioXalapa($link) {
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    function getNewsFromDiarioXalapa($url) {
+        $htmlFile = file_get_html($url); 
         
-    //     foreach($htmlFile->find("p.MsoNormal") as $new){	
+        foreach($htmlFile->find("p.MsoNormal") as $new){	
 
-    //         $c = $new->find("b", 0);
+            $c = $new->find("b", 0);
 
-    //         echo $c;
-    //     }
-    // }
+            echo $c;
+        }
+    }
 
     // //This method is for scraping for the new Mundo de Xalapa
     function getNewsFromMundoXalapa($url) {
@@ -110,230 +109,214 @@
         }
     }
 
-    // //This method is for scraping for the new Heraldo de Coatzacoalcos
-    // function getNewsFromHeraldoCoatzacoalcos($link) {
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new Heraldo de Coatzacoalcos
+    function getNewsFromHeraldoCoatzacoalcos($url) {
+        $htmlFile = file_get_html($url); 
         
-    //     foreach($htmlFile->find("div.content") as $new){	
+        foreach($htmlFile->find("div.content") as $new){	
     
-    //         $c= $new->find("p", 4);
+            $c= $new->find("p", 4);
 
-    //         echo $c;
+            echo $c;
         
-    //     } 
-    // }  
+        } 
+    }  
 
-    // //This method is for scraping for the new Diario Liberal del Sur
-    // function getNewsFromDiarioLiberal($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new Diario Liberal del Sur
+    function getNewsFromDiarioLiberal($url){
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("div.entry-body") as $new){
+        foreach($htmlFile->find("div.entry-body") as $new){
 
-    //         $c= "Discriminación en Veracruz";
+            $c= "Discriminación en Veracruz";
 
-    //         echo $c;
+            echo $c;
         
-    //     }
-    // }
+        }
+    }
 
-    // //This method is for scraping for the new El Mundo
-    // function getNewsFromElMundo($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new El Mundo
+    function getNewsFromElMundo($url){
+        $htmlFile = file_get_html($url); 
     
-    //     foreach($htmlFile->find("div.td-post-content") as $new){
+        foreach($htmlFile->find("div.td-post-content") as $new){
             
-    //         $c= $new->find("p", 1)->text();
+            $c= $new->find("p", 1)->text();
             
-    //         echo $c;
+            echo $c;
 
-    //     }
+        }
         
-    // }
+    }
 
-    // //This method is for scraping for the new El Buen Tono
-    // function getNewsFromBuenTono($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new El Buen Tono
+    function getNewsFromBuenTono($url){
+        $htmlFile = file_get_html($url); 
     
-    //     foreach($htmlFile->find("div#content") as $new){
+        foreach($htmlFile->find("div#content") as $new){
 
-    //         $c= "Discriminación en Veracruz";
+            $c= "Discriminación en Veracruz";
 
-    //         echo $c;
-    //     }
-    // }
+            echo $c;
+        }
+    }
 
-    // //This method is for scraping for the new Noticias Perfil
-    // function getNewsFromNoticiasPerfil($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new Noticias Perfil
+    function getNewsFromNoticiasPerfil($url){
+        $htmlFile = file_get_html($url); 
     
-    //     foreach($htmlFile->find("div.post") as $new){
+        foreach($htmlFile->find("div.post") as $new){
 
-    //         $c= $new->find("p", 0)->text();
-    //         echo $c;
+            $c= $new->find("p", 0)->text();
+            echo $c;
         
-    //     }
+        }
         
-    // }
+    }
 
-    // //This method is for scraping for the new Diario sin Secretos
-    // function getNewsFromDiarioSinSecretos($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new Diario sin Secretos
+    function getNewsFromDiarioSinSecretos($url){
+        $htmlFile = file_get_html($url); 
         
-    //     foreach($htmlFile->find("section#related_posts") as $new){
+        foreach($htmlFile->find("section#related_posts") as $new){
 
-    //         $c= "DISCRIMINACIÓN EN VERACRUZ";
+            $c= "DISCRIMINACIÓN EN VERACRUZ";
 
-    //         echo $c;
+            echo $c;
             
-    //     }
-    // }
+        }
+    }
 
 
-    // //This method is for scraping for the new La Politica
-    // function getNewsFromLaPolitica($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new La Politica
+    function getNewsFromLaPolitica($url){
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("div.post-content") as $new){
+        foreach($htmlFile->find("div.post-content") as $new){
 
-    //         $c= $new->find("p", 1)->text();
+            $c= $new->find("p", 1)->text();
 
-    //         echo $c;
+            echo $c;
 
-    //     }
+        }
 
-    // }
-
-
-    // //This method is for scraping for the new Vanguardia Veracruz
-    // function getNewsVanguardiaVeracruz($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
-
-    //     foreach($htmlFile->find("div.td-post-content") as $new){
-
-    //         $c= $new->find("p", 0);
-
-    //         echo $c;
-    //     }
-    // }
+    }
 
 
-    // //This method is for scraping for the new Excelsior
-    // function getNewsFromExcelsior($link) {
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new Vanguardia Veracruz
+    function getNewsVanguardiaVeracruz($url){
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("div.clear") as $new){
+        foreach($htmlFile->find("div.td-post-content") as $new){
 
-    //         $c= $new->find("p", 2);
+            $c= $new->find("p", 0);
 
-    //         echo $c;     
+            echo $c;
+        }
+    }
+
+
+    //This method is for scraping for the new Excelsior
+    function getNewsFromExcelsior($url) {
+        $htmlFile = file_get_html($url); 
+
+        foreach($htmlFile->find("div.clear") as $new){
+
+            $c= $new->find("p", 2);
+
+            echo $c;     
             
-    //     }
-    // }
+        }
+    }
 
 
-    // //This method is for scraping for the new El sol de México
-    // function getNewsFromSolDeMexico($link) {
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new El sol de México
+    function getNewsFromSolDeMexico($url) {
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("section.row") as $new){
+        foreach($htmlFile->find("section.row") as $new){
 
-    //         $c= $new->find("h1.title", 0);
+            $c= $new->find("h1.title", 0);
 
-    //         echo $c;
+            echo $c;
             
             
-    //     }
-    // }
+        }
+    }
 
 
-    // //This method is for scraping for the new Milenio
-    // function getNewsFromMilenio($link) {
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for the new Milenio
+    function getNewsFromMilenio($url) {
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("div#content-body") as $new){
+        foreach($htmlFile->find("div#content-body") as $new){
 
-    //         $c= $new->find("p", 0);
+            $c= $new->find("p", 0);
 
-    //         echo $c;
-    //     }
+            echo $c;
+        }
 
-    // }
+    }
 
-    // //This method is for scraping for Yahoo
-    // function getNewsFromYahoo($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for Yahoo
+    function getNewsFromYahoo($url){
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("li.first") as $new){
+        foreach($htmlFile->find("li.first") as $new){
 
-    //         $c= "Discriminación en el estado de Veracruz";
+            $c= "Discriminación en el estado de Veracruz";
 
-    //         echo $c;
-    //     }
-    // }
+            echo $c;
+        }
+    }
 
-    // //This method is for scraping for Twitter
-    // function getNewsFromTwitter($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for Twitter
+    function getNewsFromTwitter($url){
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("li.js-stream-item") as $new){
+        foreach($htmlFile->find("li.js-stream-item") as $new){
             
-    //         $c= "Resultado de Discriminación en el estado de Veracruz ";
+            $c= "Resultado de Discriminación en el estado de Veracruz ";
             
-    //         echo $c;
+            echo $c;
             
-    //     }
-    // }
+        }
+    }
     
-    // //This method is for scraping for Bing
-    // function getNewsFromBing($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for Bing
+    function getNewsFromBing($url){
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("div.snippet") as $new){
+        foreach($htmlFile->find("div.snippet") as $new){
 
-    //         $c= "Discriminan en veracruz";
-    //         echo $c;
-    //     }
-    // }
+            $c= "Discriminan en veracruz";
+            echo $c;
+        }
+    }
 
-    // //This method is for scraping for Google
-    // function getNewsFromGoogle($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for Google
+    function getNewsFromGoogle($url){
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("div.g") as $new){
+        foreach($htmlFile->find("div.g") as $new){
 
-    //         $c= "Resultado de Discriminación en el estado de Veracruz ";
+            $c= "Resultado de Discriminación en el estado de Veracruz ";
             
-    //         echo $c;
+            echo $c;
             
-    //     }
-    // }  
+        }
+    }  
 
-    // //This method is for scraping for Imagen Veracruz
-    // function getNewsFromImagenVeracruz($link){
-    //     $link = $url;
-    //     $htmlFile = file_get_html($url); 
+    //This method is for scraping for Imagen Veracruz
+    function getNewsFromImagenVeracruz($url){
+        $htmlFile = file_get_html($url); 
 
-    //     foreach($htmlFile->find("circle.path") as $new){
+        foreach($htmlFile->find("circle.path") as $new){
            
-    //         $c= "Resultado de Discriminación en el estado de Veracruz ";
+            $c= "Resultado de Discriminación en el estado de Veracruz ";
             
-    //         echo $c;
+            echo $c;
             
-    //     }
-    // } 
+        }
+    } 
 ?>
